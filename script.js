46777af7d81e190d3ebf886a292bde49bb32415c
agendavictor24-netizen <agendavictor24@gmail.com>
2026-09-05 21:49:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector("form").addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert("Thank you for choosing Victor Travel & Tours! We shall contact you shortly.");
+
+this.reset();
+
+});
